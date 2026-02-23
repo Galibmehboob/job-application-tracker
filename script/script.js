81d6dce1cdae799e7252b2d0b0c3224912052241
Galib Mehboob <86629449+Galibmehboob@users.noreply.github.com>
2026-02-23 19:55:55 +0600

    Responsive mobile

--- script/script.js
+++ script/script.js
@@ -1,1 +1,16 @@
-console.log('Hello');
+// console.log('Hello interviewers');
+
+let total = document.getElementById('total');
+let interview = document.getElementById('interview');
+let rejected = document.getElementById('rejected');
+let sideBarJobs = document.getElementById('sideJobsBar');
+// console.log(rejected);
+
+const allbtn = document.getElementById('allBtn');
+const interviewbtn = document.getElementById('interviewBtn');
+const rejectebtn = document.getElementById('rejectBtn');
+
+const allJobsSection = document.getElementById('allJobs');
+const mainContainer = document.querySelector('main');
+const filterSection = document.getElementById('filterSection');
+// console.log(mainContainer);
