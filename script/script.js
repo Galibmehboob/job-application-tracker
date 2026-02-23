9761e15f2a111f2d3da4ef24889075839c8016ca
Galib Mehboob <86629449+Galibmehboob@users.noreply.github.com>
2026-02-23 22:08:04 +0600

    button toggler added

--- script/script.js
+++ script/script.js
@@ -1,4 +1,7 @@
-// console.log('Hello interviewers');
+// console.log('Hello clicked');
+let interviewlist = [];
+let rejectedlist = [];
+let currentStatus = 'all';
 
 let total = document.getElementById('total');
 let interview = document.getElementById('interview');
@@ -14,3 +17,48 @@ const allJobsSection = document.getElementById('allJobs');
 const mainContainer = document.querySelector('main');
 const filterSection = document.getElementById('filterSection');
 // console.log(mainContainer);
+
+// Dashboard counter update
+function calculateCount() {
+    const totalJobs = allJobsSection.children.length;
+    total.innerText = totalJobs;//8
+    interview.innerText = interviewlist.length;
+    rejected.innerText = rejectedlist.length;
+    sideBarJobs.innerText = allJobsSection.children.length;//8
+
+    // Sidebar jobs dynamic
+    if (currentStatus === 'allBtn') {
+        sideBarJobs.innerText = `${totalJobs} jobs`; // all button always total
+    } else if (currentStatus === 'interviewBtn') {
+        sideBarJobs.innerText = `${interviewlist.length} of ${totalJobs} jobs`;
+    } else if (currentStatus === 'rejectBtn') {
+        sideBarJobs.innerText = `${rejectedlist.length} of ${totalJobs} jobs`;
+    }
+}
+calculateCount();
+
+//Toggle filter buttons 
+function toggle(id) {
+    allbtn.classList.remove('btn-primary');
+    interviewbtn.classList.remove('btn-primary');
+    rejectebtn.classList.remove('btn-primary');
+
+    document.getElementById(id).classList.add('btn-primary');
+    currentStatus = id;
+
+    if (id == 'allBtn') {
+        allJobsSection.classList.remove('hidden');
+        filterSection.classList.add('hidden');
+    } else if (id == 'interviewBtn') {
+        allJobsSection.classList.add('hidden');
+        filterSection.classList.remove('hidden');
+        renderListOrEmpty(interviewlist);
+    } else if (id == 'rejectBtn') {
+        allJobsSection.classList.add('hidden');
+        filterSection.classList.remove('hidden');
+        renderListOrEmpty(rejectedlist);
+    }
+
+    calculateCount();
+}
+
