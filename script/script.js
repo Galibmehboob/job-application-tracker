ffc3020ecbdc9b40f4a1afaa34be4b3a01235291
Galib Mehboob <86629449+Galibmehboob@users.noreply.github.com>
2026-02-23 22:52:07 +0600

    revice done all ok

--- script/script.js
+++ script/script.js
@@ -143,3 +143,50 @@ mainContainer.addEventListener('click', function (event) {
 
 });
 
+//  No jobs available
+function renderListOrEmpty(list) {
+    filterSection.innerHTML = '';
+
+    if (list.length === 0) {
+        // Empty  card
+        const div = document.createElement('div');
+        div.className = 'flex flex-col items-center justify-center bg-indigo-50 rounded-md p-6 mt-6 h-[400px] shadow-lg text-center';
+        div.innerHTML = `
+            <i class="fa-solid fa-file text-blue-500 text-4xl mb-4"></i>
+            <p class="text-black font-bold text-xl mb-2">No jobs available</p>
+            <p class="text-gray-400 text-sm">Check back soon for new job opportunities</p>
+        `;
+        filterSection.appendChild(div);
+        return;
+    }
+
+    // Otherwise render normal cards
+    list.forEach(item => {
+        const div = document.createElement('div');
+        div.className = 'flex justify-between bg-indigo-50 rounded-md p-6 mt-6 shadow-lg';
+        div.innerHTML = `
+<div class="space-y-5">
+    <div>
+        <p class="jobName font-bold text-xl">${item.jobName}</p>
+        <p class="paraTitle text-gray-500 text-[15px]">${item.paraTitle}</p>
+    </div>
+    <p class="salary text-gray-500 font-semibold text-sm">${item.salary}</p>
+    <p class="applied text-[14px] w-24 text-center p-2 ${item.applied === 'Interview' ? 'text-green-700 bg-green-200' : 'text-red-700 bg-red-200'}">${item.applied}</p>
+    <p class="sub-title text-gray-600 text-[15px]">${item.subTitle}</p>
+    <div>
+        <button class="btn btn-outline btn-success font-bold">interview</button>
+        <button class="btn btn-outline btn-error font-bold">Rejected</button>
+    </div>
+</div>
+<div>
+    <button class="btn btn-circle hover:bg-red-300">
+        <i class="fa-regular fa-trash-can"></i>
+    </button>
+</div>
+        `;
+        filterSection.appendChild(div);
+    });
+}
+
+//counts on load
+calculateCount();
