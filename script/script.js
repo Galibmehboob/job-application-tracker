5686f515ec1ee4d4546da33bba1d55510bfb61cd
Galib Mehboob <86629449+Galibmehboob@users.noreply.github.com>
2026-02-23 22:50:00 +0600

    eventListener

--- script/script.js
+++ script/script.js
@@ -94,5 +94,52 @@ mainContainer.addEventListener('click', function (event) {
             appliedEl.classList.add('text-red-700', 'bg-red-200');
             appliedEl.classList.remove('text-gray-800', 'bg-gray-200', 'text-green-700', 'bg-green-200');
         }
+
+        // Add> interview  rejected 
+        if (appliedStatus === 'Interview') {
+            if (!interviewlist.find(item => item.jobName === jobName))
+                interviewlist.push(cardInfo);
+            rejectedlist = rejectedlist.filter(item => item.jobName !== jobName);
+        } else if (!rejectedlist.find(item => item.jobName === jobName)) {
+            rejectedlist.push(cardInfo);
+            interviewlist = interviewlist.filter(i => i.jobName !== jobName);
+        }
+        toggle(currentStatus);
+        calculateCount();
+
+        // If current filter active, render
+        if ((currentStatus === 'interviewBtn' && appliedStatus === 'Interview') ||
+            (currentStatus === 'rejectBtn' && appliedStatus === 'Rejected')) {
+            renderListOrEmpty(appliedStatus === 'Interview' ? interviewlist : rejectedlist);
+        } else if (currentStatus === 'allBtn') {
+            filterSection.classList.add('hidden');
+            allJobsSection.classList.remove('hidden');
+        }
     }
-})
+
+    // Delete Job 
+    else if (event.target.closest('.btn-circle')) {
+        const parentCard = event.target.closest('#allJobs > div, #filterSection > div');
+        if (!parentCard) return;
+
+        const jobName = parentCard.querySelector('.jobName').innerText;
+
+        //dlt  from interview & rejected page
+        interviewlist = interviewlist.filter(item => item.jobName !== jobName);
+        rejectedlist = rejectedlist.filter(item => item.jobName !== jobName);
+
+        // dlt from DOM
+        parentCard.remove();
+
+        calculateCount();
+
+        //  if active
+        if (currentStatus === 'interviewBtn') {
+            renderListOrEmpty(interviewlist);
+        } else if (currentStatus === 'rejectBtn') {
+            renderListOrEmpty(rejectedlist);
+        }
+    }
+
+});
+
